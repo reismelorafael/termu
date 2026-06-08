021f0d40a5756e012e5b8b14c9068a6537b93f79
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-06-08 07:30:30 -0300

    fix: fail fast on incomplete Termux initialization

--- app/src/main/java/com/termux/app/TermuxApplication.java
+++ app/src/main/java/com/termux/app/TermuxApplication.java
@@ -10,9 +10,4 @@ import com.termux.shared.termux.TermuxBootstrap;
 import com.termux.shared.termux.TermuxConstants;
 import com.termux.shared.termux.crash.TermuxCrashUtils;
 import com.termux.shared.termux.file.TermuxFileUtils;
-import com.termux.shared.termux.settings.preferences.TermuxAppSharedPreferences;
-import com.termux.shared.termux.settings.properties.TermuxAppSharedProperties;
-import com.termux.shared.termux.shell.command.environment.TermuxShellEnvironment;
-import com.termux.shared.termux.shell.am.TermuxAmSocketServer;
-import com.termux.shared.termux.shell.TermuxShellManager;
-import com.termux.shared.termux.theme.TermuxThemeUtils;
+import com.termux.shared
